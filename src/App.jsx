2b1e4515edcc735d3bb2e8d0632cf5462ecadf67
unknown <yavtushenkon@gmail.com>
2024-12-22 22:08:31 +0200

    contactform

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import ContactForm from './components/ContactForm/ContactForm';
+import ContactForm from './components/ContactForm/ContactForm'
 import ContactList from './components/ContactList/ContactList';
 import SearchBox from './components/SearchBox/SearchBox'
 
