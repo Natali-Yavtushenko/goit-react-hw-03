2ae2a43780accc0cf77f6a75bbd31e3a63770caf
unknown <yavtushenkon@gmail.com>
2024-12-22 21:49:57 +0200

    register

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -1,71 +1,85 @@
 import s from './ContactForm.module.css';
 import React from 'react';
-import {Formik, Field, Form, ErrorMessage } from 'formik';
-import {useId} from "react";
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { useId } from 'react';
 import * as Yup from 'yup';
 
-const initialValues={
-    Name: '',
-    Number: '',
+const initialValues = {
+  Name: '',
+  Number: '',
+};
+
+const ContactForm = ({ addContact }) => {
+  const NameFieldId = useId();
+  const NumberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    addContact({
+      id: Date.now().toString(),
+      name: values.Name,
+      number: values.Number,
+    });
+    actions.resetForm();
   };
 
-
-const  ContactForm = ({ addContact }) => {
-   const NameFieldId = useId();
-   const NumberFieldId = useId();
-
-   const handleSubmit = (values, actions,) => {
-    console.log(values);
-    
-    addContact({
-        id: Date.now().toString(),
-        name: values.Name,
-        number: values.Number,
-       });
-       actions.resetForm();
-   };
-
-   const onlyLetters = /^[A-Za-zА-Яа-яЄєІіЇїҐґ]+$/;
-   const registerSchema = Yup.object().shape({
-       Name: Yup.string()
+  
+  const registerSchema = Yup.object().shape({
+    Name: Yup.string()
       .min(3, 'Мінімум 3 символи')
       .max(20, 'Максимум 20 символів')
-      .matches(onlyLetters, 'Це поле може містити тільки літери')
+      .matches(/^[A-Za-zА-Яа-яЄєІіЇїҐґ]+$/, 'Це поле може містити тільки літери')
       .required("Це поле обов'язкове!"),
-      Number: Yup.number()
-      .min(7, 'Мінімум 7 символів')
-      .max(13, 'Максимуму 13 символів')
-      .positive(),
+    Number: Yup.string()
+    .min(7, 'Мінімум 7 символів')
+    .max(13, 'Максимуму 13 символів')
+      .required("Це поле обов'язкове!"),
   });
-    
-  
 
-return (
+  return (
     <div className={s.formWrapper}>
-    <Formik initialValues={initialValues}
-    validationSchema={registerSchema}
-    onSubmit={handleSubmit}
-  >
-     {({ errors, touched }) => (
-  <Form className={s.form}>
-  <label className={s.label} htmlFor={NameFieldId}>Name</label>
-  <Field className={s.field} id={NameFieldId} name="Name" placeholder="Anna"/>
-  {errors.name && touched.name ? (
-    <div>{errors.name}</div>
-     ) : null}
-     <ErrorMessage name="name" />
-  <label className={s.label} htmlFor={NumberFieldId}>Number</label>
-  <Field className={s.field} id={NumberFieldId} name="Number" placeholder="720-35-78"/>
-  {errors.number && touched.number ? (
-     <div>{errors.number}</div>
-    ) : null}
-    <ErrorMessage name="Number" />
-  <button className={s.button} type="submit">Add contact</button>
-  </Form>
-       )}
-     </Formik>
-   </div>
-);
-}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={registerSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ errors, touched }) => (
+          <Form className={s.form}>
+            <label className={s.label} htmlFor={NameFieldId}>
+              Name
+            </label>
+            <Field
+              className={s.field}
+              id={NameFieldId}
+              name="Name"
+              placeholder="Anna"
+            />
+            {errors.Name && touched.Name ? <div>{errors.Name}</div> : null}
+            <ErrorMessage name="Name" component="div" />
+
+            <label className={s.label} htmlFor={NumberFieldId}>
+              Number
+            </label>
+            <Field
+              className={s.field}
+              id={NumberFieldId}
+              name="Number"
+              placeholder="7203578"
+              type="tel"
+              inputMode="numeric"
+              pattern="\d*"
+            />
+            {errors.Number && touched.Number ? <div>{errors.Number}</div> : null}
+            <ErrorMessage name="Number" component="div" />
+
+            <button className={s.button} type="submit">
+              Add contact
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default ContactForm;
